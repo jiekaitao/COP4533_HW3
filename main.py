70b51266477cd181bb1e03754ab04153b1b8cdda
Jack Tao <jietao@ufl.edu>
2026-04-06 16:47:43 -0400

    Switched to an actually recursive approach

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def parse_input(input_str: str) -> tuple[int, str, str, dict[str, str]]:
     K = int(lines[0])
 
     str_dict = {}
-    for i in range(1, K-2):
+    for i in range(1, K-1):
         temp_row = lines[i].split(" ")
         str_dict[temp_row[0]] = temp_row[1]
     
@@ -12,22 +12,26 @@ def parse_input(input_str: str) -> tuple[int, str, str, dict[str, str]]:
 
     return K, A, B, str_dict
 
-def score_subsequence(A_sub: str, B_sub: str, str_dict = dict[str, str]) -> int:
-   for i in A_sub:
-       for j in B_sub:
-           if 
-
 
 
 def main(input_str: str) -> tuple[int, str]:
     K, A, B, str_dict = parse_input(input_str)
     # build 2d table where each entry (i,j) is the maximal value of the subsequence that would be realized upon using the first 
     # i characters of A and the first j characters of B
-
     table = [[]]
+
+    # base case: both strings are empty
+    for i in range(len(A)):
+        table[i][0] = 0
+    
+    for j in range(len(B)):
+        table[0][j] = 0
+
+    
     for i in range(len(A)):
         for j in range(len(B)):
-            score_subsequence(A[:i], B[:j])
+
+
 
 
 
