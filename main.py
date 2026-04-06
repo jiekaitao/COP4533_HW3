d85c7438de8a491dec2f764e888313f21cb37bd3
Jack Tao <jietao@ufl.edu>
2026-04-06 18:15:09 -0400

    Fixed indices

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def parse_input(input_str: str) -> tuple[int, str, str, dict[str, str]]:
+def parse_input(input_str: str) -> tuple[int, str, str, dict[str, int]]:
     lines = input_str.split("\n")
     K = int(lines[0])
 
     str_dict = {}
-    for i in range(1, K-1):
+    for i in range(1, len(lines)-2):
         temp_row = lines[i].split(" ")
-        str_dict[temp_row[0]] = temp_row[1]
+        str_dict[temp_row[0]] = int(temp_row[1])
     
     A = lines[K-1]
     B = lines[K]
@@ -18,20 +18,24 @@ def main(input_str: str) -> tuple[int, str]:
     K, A, B, str_dict = parse_input(input_str)
     # build 2d table where each entry (i,j) is the maximal value of the subsequence that would be realized upon using the first 
     # i characters of A and the first j characters of B
-    table = [[]]
-
-    # base case: both strings are empty
+    table = []
     for i in range(len(A) + 1):
-        table[i][0] = 0
-    
-    for j in range(len(B) + 1):
-        table[0][j] = 0
+        row = []
+        for j in range(len(B) + 1):
+            row.append(0)
+        table.append(row)
 
+    # # base case: both strings are empty
+    # for i in range(len(A) + 1):
+    #     table[i][0] = 0
     
-    for i in A:
-        for j in B:
-            if i == j:
-                table[i+1][j+1] = str_dict[i] # one indexed because of the base cases
+    # for j in range(len(B) + 1):
+    #     table[0][j] = 0
+    
+    for i in range(len(A)):
+        for j in range(len(B)):
+            if A[i] == B[j]:
+                table[i+1][j+1] = table[i][j] + str_dict[A[i]] # one indexed because of the base cases
             else:
                 table[i+1][j+1] = max(table[i][j+1], table[i+1][j])
         
