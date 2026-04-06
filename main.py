2b0cb96c60f57b7bccc248c1110ddac51f80207c
Jack Tao <jietao@ufl.edu>
2026-04-06 16:37:24 -0400

    Started on score_subsequence function

--- main.py
+++ main.py
@@ -11,3 +11,23 @@ def parse_input(input_str: str) -> tuple[int, str, str, dict[str, str]]:
     B = lines[K]
 
     return K, A, B, str_dict
+
+def score_subsequence(A_sub: str, B_sub: str, str_dict = dict[str, str]) -> int:
+   for i in A_sub:
+       for j in B_sub:
+           if 
+
+
+
+def main(input_str: str) -> tuple[int, str]:
+    K, A, B, str_dict = parse_input(input_str)
+    # build 2d table where each entry (i,j) is the maximal value of the subsequence that would be realized upon using the first 
+    # i characters of A and the first j characters of B
+
+    table = [[]]
+    for i in range(len(A)):
+        for j in range(len(B)):
+            score_subsequence(A[:i], B[:j])
+
+
+
